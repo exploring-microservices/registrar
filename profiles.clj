1c05900956dc4678dcfda0189cf64aa7e6f9ccdf
Sandro Padin <sandropadin@gmail.com>
2015-02-26 21:20:21 -0600

    Use long form development instead of dev

--- profiles.clj
+++ profiles.clj
@@ -1,10 +1,10 @@
 {:dev {:jvm-opts ["-Dlogfile.name=development.log"]
-       :env {:clj-env "dev"
+       :env {:clj-env "development"
              :db-classname "org.j2.Driver"
              :db-subprotocol "h2"
-             :db-subname "./resources/db/dev"}
+             :db-subname "./resources/db/development"}
        :ragtime {:migrations ragtime.sql.files/migrations
-                 :database "jdbc:h2:./resources/db/dev"}}
+                 :database "jdbc:h2:./resources/db/development"}}
  :test {:jvm-opts ["-Dlogfile.name=test.log"]
         :env {:clj-env "test"
               :db-classname "org.j2.Driver"
