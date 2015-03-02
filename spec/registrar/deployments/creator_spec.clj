5c9363bd383ab8652bc1eedde7501d7c88909923
Sandro Padin <sandropadin@gmail.com>
2015-03-01 19:01:26 -0600

    Check that deployment is persisted

--- spec/registrar/deployments/creator_spec.clj
+++ spec/registrar/deployments/creator_spec.clj
@@ -2,7 +2,8 @@
   (:require [speclj.core :refer [context describe it
                                  should= should-not-be-nil]]
             [spec-helper :refer [with-transactions]]
-            [registrar.deployments.creator :refer :all]))
+            [registrar.deployments.creator :refer :all]
+            [registrar.deployments.fetcher :as fetcher]))
 
 (describe "registrar.deployments.creator"
   (with-transactions)
@@ -11,4 +12,9 @@
     (it "returns the newly created deployment"
       (let [deployment (create {:name "website.com"})]
         (should= "website.com" (:name deployment))
-        (should-not-be-nil (:id deployment))))))
+        (should-not-be-nil (:id deployment))))
+
+    (it "persists the newly created deployment"
+      (let [deployment (create {:name "website.com"})
+            found-deployment (fetcher/fetch-by-id (:id deployment))]
+        (should= deployment found-deployment)))))
